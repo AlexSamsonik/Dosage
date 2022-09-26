3f7c7e2ebbc2c797c8ce37826f74648c5baa64e5
Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>
2022-09-26 15:44:25 +0200

    Paracetamol dosage 120mg/5ml has been added.

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ Config.set("kivy", "keyboard_mode", "system")
 
 
 def get_dosages(mass):
-    paracetamol = str(round(15 * mass / 1000 * 5 / 240, 2))
-    ibuprofen = str(round(10 * mass / 1000 * 5 / 100, 2))
-    return {"paracetamol": paracetamol, "ibuprofen": ibuprofen}
+    paracetamol_240 = str(round(15 * mass / 1000 * 5 / 240, 2))
+    paracetamol_120 = str(round(15 * mass / 1000 * 5 / 120, 2))
+    ibuprofen = str(round(10 * mass / 1000 * 5 / 200, 2))
+    return {"paracetamol_240": paracetamol_240,
+            "paracetamol_120": paracetamol_120,
+            "ibuprofen": ibuprofen}
 
 
 class Container(GridLayout):
@@ -20,7 +23,8 @@ class Container(GridLayout):
 
         dosages = get_dosages(mass)
 
-        self.paracetamol.text = dosages.get("paracetamol")
+        self.paracetamol_240.text = dosages.get("paracetamol_240")
+        self.paracetamol_120.text = dosages.get("paracetamol_120")
         self.ibuprofen.text = dosages.get("ibuprofen")
 
 
